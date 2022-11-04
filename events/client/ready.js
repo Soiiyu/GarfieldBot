32dd71c0c8713473ed18af4430c0413deb0adb22
a1alex2 <sania.rabinov@gmail.com>
2022-11-04 23:46:01 +0200

    +activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,10 @@
+const { ActivityType } = require('discord.js');
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
         console.log(`Ready as ${client.user.tag} 🐈`)
+
+        client.user.setActivity('to MeowRemix.wav', { type: ActivityType.Listening })
     }
 }
