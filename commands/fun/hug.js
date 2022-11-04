ccaf767c684b837ed743432a0750cc3d8c27a566
a1alex2 <sania.rabinov@gmail.com>
2022-11-04 21:11:11 +0200

    +hug command

---
+++ commands/fun/hug.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const slaps = require('./hugs.json')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hug')
+        .setDescription('Hug someone!')
+        .addUserOption(option => 
+            option
+                .setName('user')
+                .setDescription('The user to hug')
+                .setRequired(true)),
+    async execute(interaction, client) {
+        const user = interaction.options.getUser('user');
+        const randomHug = hugs[Math.floor(Math.random() * hugs.length)]
+        const embed = new EmbedBuilder()
+            .setImage(randomHug)
+            .setColor(client.color)
+            .setDescription(`${interaction.user} hugged ${user}!`)
+        await interaction.reply({embeds: [embed]})
+    }
+}
