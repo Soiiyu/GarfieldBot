b8cd790cec055150035c285975729236e3a59fd6
Soiiyu <soiiyukoto@gmail.com>
2022-11-04 18:42:03 +0200

    Slap users

--- commands/fun/slap.js
+++ commands/fun/slap.js
@@ -4,9 +4,15 @@ const slaps = require('./slaps.json')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('slap')
-        .setDescription('Slap someone!'),
+        .setDescription('Slap someone!')
+        .addUserOption(option => 
+            option
+                .setName('user')
+                .setDescription('The user to slap')
+                .setRequired(true)),
     async execute(interaction, client) {
+        const user = interaction.options.getUser('user');
         let randomSlap = slaps[Math.floor(Math.random() * slaps.length)]
-        await interaction.reply({content: randomSlap})
+        await interaction.reply({content: `<@${interaction.user.id}> slapped <@${user.id}>\n${randomSlap}`})
     }
 }
