da7606d11397122a9e7cb7f6915d7e5d611e5f50
Soiiyu <soiiyukoto@gmail.com>
2022-11-05 19:29:28 +0200

    made poll work, moved to utility folder

--- commands/utility/poll.js
+++ commands/utility/poll.js
@@ -6,11 +6,12 @@ module.exports = {
         .setDescription('Make a poll!')
         .addStringOption(option => 
             option
-                .setName("Poll's title")
+                .setName("title")
                 .setDescription('The title of the poll')
                 .setRequired(true)),
-    async execute(interaction) {
-        const desc = interaction.option.getString("Poll's title")
+    async execute(interaction, client) {
+        const desc = interaction.options.getString("title")
+        console.log(desc)
         const embed = new EmbedBuilder()
             .setImage('https://i.imgur.com/Cax8Kw2.png')
             .setColor(client.color)
