088caf911d586be7c907d3f8874d07f98fb5d763
Soiiyu <soiiyukoto@gmail.com>
2023-07-25 17:10:53 +0300

    endpoll troubleshooting

--- functions/helpers/endpoll.js
+++ functions/helpers/endpoll.js
@@ -12,44 +12,49 @@ module.exports = client => {
                 message = null
             }
         }
-        
+
         if (message) { // if the message was not manually deleted
             // Generating poll results and updating the embed 
-            const embed = message.embeds[0]
-            const highestVotes = [0]
-            const totalVotes = Math.max(1, poll.votes.reduce((votes, curr) => {
-                if (curr.length > highestVotes[0]) highestVotes[0] = curr.length
-                return votes + curr.length
-            }, 0))
-            switch (poll.pollType) {
-                case 'yesno':
-                    const [yes, no] = poll.votes
-                    embed.data.description = `${client.boldText(`<:Yes:712682828302909481> ${Math.round(100 * yes.length / totalVotes)}%`, yes.length > no.length)} - ${client.boldText(`<:No:712682828332138586> ${Math.round(100 * no.length / totalVotes)}%`, no.length > yes.length)}`
-                    break
-                case 'buttons':
-                case 'selectmenu':
-                    embed.data.description = embed.data.description
-                        .split('\n')
-                        .map((option, i) => client.boldText(`[${Math.round(100 * poll.votes[i].length / totalVotes)}%] ${option}`, poll.votes[i].length == highestVotes[0] && poll.votes[i].length !== 0))
-                        .join('\n')
-                    break
-            }
-
-            // embed.data.footer = { text: 'poll ended' }
-            // updating the "End in" field
-            message.embeds[0].fields[0].name = 'Ended'
-            message.embeds[0].fields[0].value = `<t:${Math.round(Date.now() / 1000)}:R>`
-
-            // Disable 'end poll' button
-            message.components[1].components[0].data.disabled = true
-
-            // Fetched messages use .edit() instead of .update(), this is to handle the difference
             try {
-                if (typeof interaction.update == 'function') await interaction.update({ embeds: [embed], components: [message.components[1]] })
-                else await interaction.edit({ embeds: [embed], components: [message.components[1]] })
-            } catch (error) {
-                console.log('could not edit poll message', error)
+                const embed = message.embeds[0]
+                const highestVotes = [0]
+                const totalVotes = Math.max(1, poll.votes.reduce((votes, curr) => {
+                    if (curr.length > highestVotes[0]) highestVotes[0] = curr.length
+                    return votes + curr.length
+                }, 0))
+                switch (poll.pollType) {
+                    case 'yesno':
+                        const [yes, no] = poll.votes
+                        embed.data.description = `${client.boldText(`<:Yes:712682828302909481> ${Math.round(100 * yes.length / totalVotes)}%`, yes.length > no.length)} - ${client.boldText(`<:No:712682828332138586> ${Math.round(100 * no.length / totalVotes)}%`, no.length > yes.length)}`
+                        break
+                    case 'buttons':
+                    case 'selectmenu':
+                        embed.data.description = embed.data.description
+                            .split('\n')
+                            .map((option, i) => client.boldText(`[${Math.round(100 * poll.votes[i].length / totalVotes)}%] ${option}`, poll.votes[i].length == highestVotes[0] && poll.votes[i].length !== 0))
+                            .join('\n')
+                        break
+                }
+
+                // embed.data.footer = { text: 'poll ended' }
+                // updating the "End in" field
+                message.embeds[0].fields[0].name = 'Ended'
+                message.embeds[0].fields[0].value = `<t:${Math.round(Date.now() / 1000)}:R>`
+
+                // Disable 'end poll' button
+                message.components[1].components[0].data.disabled = true
+
+                // Fetched messages use .edit() instead of .update(), this is to handle the difference
+                try {
+                    if (typeof interaction.update == 'function') await interaction.update({ embeds: [embed], components: [message.components[1]] })
+                    else await interaction.edit({ embeds: [embed], components: [message.components[1]] })
+                } catch (error) {
+                    console.log('could not edit poll message', error)
+                }
+            } catch (msgError) {
+                console.log('Error ending poll', msgError)
             }
+
 
         }
 
